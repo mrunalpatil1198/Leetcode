7b250eb3d105a3fedfc5e17b8a4e375965dd0162
Mrunal Patil <mrupatil@users.noreply.github.com>
2023-07-09 10:45:35 -0400

    Leetcode27

---
+++ 27-RemoveElement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        insert_at = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[insert_at] = nums[i]
+                insert_at += 1
+        return insert_at
+    
+if __name__ == '__main__':
+    s = Solution()
+    print(s.removeElement(nums = [3,2,2,3], val = 3))
