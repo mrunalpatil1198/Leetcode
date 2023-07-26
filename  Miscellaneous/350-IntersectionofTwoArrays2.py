6f8582ac21c41194b9cc5ba030cd3937a546bc4c
Mrunal Patil <mrunalpatil1198@gmail.com>
2023-07-26 16:38:34 -0400

    add 350

---  Miscellaneous/350-IntersectionofTwoArrays2.py
+++  Miscellaneous/350-IntersectionofTwoArrays2.py
@@ -4,10 +4,12 @@ class Solution:
         n_1 = collections.defaultdict(int)
         result = []
 
+        #create a counter dict for nums1
         for num in nums1:
             n_1[num] += 1
 
-        for num in nums2:
+        #check for all nums in nums2 if they are present in nums1
+        for num in nums2: 
             if num in n_1 and n_1[num] != 0:
                 result.append(num)
                 n_1[num] -= 1
@@ -19,3 +21,5 @@ if __name__ == '__main__':
     print(s.intersect(nums1 = [1,2,2,1], nums2 = [2,2]))
 
             
+#Time Complexity - O(n) where n = max(len(nums1), len(nums2))
+#Space Complexity - O(n) where n = len(nums1)
