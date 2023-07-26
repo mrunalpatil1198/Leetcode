28bbdd40653805cc2eec37b81bf5ce766d28cb5b
Mrunal Patil <mrunalpatil1198@gmail.com>
2023-07-26 16:44:06 -0400

    add 414

---  Miscellaneous/414-ThirdMaximumNumber.py
+++  Miscellaneous/414-ThirdMaximumNumber.py
@@ -1,11 +1,18 @@
 class Solution:
     def thirdMax(self, nums: list[int]) -> int:
+
+        #create set of nums to avoid duplicates
         distincts = set(nums)
+
         if len(distincts) < 3:
             return max(distincts)
         else: 
+            #sort and return the third last element
             return sorted(list(distincts))[-3]
         
 if __name__ == '__main__':
     s = Solution()
     print(s.thirdMax([3,2,1]))
+
+#Time Complexity - O(nlogn)
+#Space Complexity - O(n)
