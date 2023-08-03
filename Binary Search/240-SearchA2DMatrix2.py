b75bf2793d82f8abab4a52d8de8284f1b19aa8d9
Mrunal Patil <mrunalpatil1198@gmail.com>
2023-08-03 16:55:28 -0400

    add 240

---
+++ Binary Search/240-SearchA2DMatrix2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        i = 0
+        j = len(matrix[0]) - 1
+
+        while i < len(matrix) and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                j -= 1
+            else:
+                i += 1
+        
+        return False
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5))
